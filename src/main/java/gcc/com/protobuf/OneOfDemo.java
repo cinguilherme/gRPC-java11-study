1fd2f7fed0b235e14e7944b12d30b90a924ef241
cinguilherme <cinguilherme@gmail.com>
2021-10-28 22:59:23 -0300

    creds, oneof demo

---
+++ src/main/java/gcc/com/protobuf/OneOfDemo.java
@@ -0,0 +1,34 @@
+package gcc.com.protobuf;
+
+import gcc.com.models.Credentials;
+import gcc.com.models.EmailCredentials;
+import gcc.com.models.PhoneOTP;
+
+public class OneOfDemo {
+
+    public static void main(String[] args) {
+
+        EmailCredentials emailCredentials = EmailCredentials.newBuilder()
+                .setEmail("noone@nowhere.com")
+                .setPassword("password")
+                .build();
+
+        PhoneOTP phoneCreds = PhoneOTP.newBuilder()
+                .setCode(123456)
+                .setNumber(84578475)
+                .build();
+
+        //last one set on builder goes!
+        Credentials creds = Credentials.newBuilder()
+                .setPhoneMode(phoneCreds)
+                .setEmailMode(emailCredentials)
+                .build();
+
+        loggin(creds);
+
+    }
+
+    private static void loggin(Credentials credentials) {
+        System.out.println(credentials);
+    }
+}
